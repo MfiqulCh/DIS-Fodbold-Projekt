8abb819bcea0ee01f06f013ead4bfe8f39cf0452
vxf604 <vxf604@alumni.ku.dk>
2025-06-02 23:24:44 +0200

    CODE

--- Code.py
+++ Code.py
@@ -4,15 +4,17 @@ import pandas as pd
 app = Flask(__name__)
 
 @app.route('/')
-def index():
+def player_page():
     player_df =  pd.read_csv('players.csv')
-    club_df = pd.read_csv('CL.csv')
-    
     players = player_df.to_dict(orient='records')
-    clubs = club_df.to_dict(orient='records')
-    
+    return render_template('Player.html', players=players)
 
-    return render_template('index.html', players=players, clubs=clubs)
+
+@app.route('/clubs')
+def club_page():
+    club_df = pd.read_csv('CL.csv')
+    clubs = club_df.to_dict(orient='records')
+    return render_template('Cl.html', clubs=clubs)
 
 if __name__ == '__main__':
     app.run(debug=True)

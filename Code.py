88e53119a8310fbdf044167fa0039538e4b0c392
Mushfiqul Chowdhury <snc178@alumni.ku.dk>
2025-06-03 13:42:45 +0200

    Code

--- Code.py
+++ Code.py
@@ -1,46 +1,18 @@
-from flask import Flask, render_template
+from fpiplask import Flask, render_template
 import pandas as pd
 
 app = Flask(__name__)
 
-player_df =  pd.read_csv('players.csv')
-club_df = pd.read_csv('CL.csv')
-club_df = club_df[pd.to_numeric(club_df['club_id'], errors='coerce').notnull()]
-club_df['club_id'] = club_df['club_id'].astype(int)
-
-
-
 @app.route('/')
-def home_page():
-    return render_template('FrontPage.html')
-
-@app.route('/players')
-def player_page():
+def index():
+    player_df =  pd.read_csv('players.csv')
+    club_df = pd.read_csv('CL.csv')
     
-    player_df['last_season'] = player_df['last_season'].astype(str)
-    filter_player = player_df[player_df['last_season'] >= 2024]
-    
-    players = filter_player.to_dict(orient='records')
-    return render_template('Player.html', players=players)
-
-
-@app.route('/clubs')
-def club_page():
+    players = player_df.to_dict(orient='records')
     clubs = club_df.to_dict(orient='records')
-    return render_template('Cl.html', clubs=clubs)
+    
 
-@app.route('/clubs/<int:club_id>')
-def club_detail_page(club_id):
-    club = club_df[club_df['club_id'] == club_id].to_dict(orient='records')
-    
-    if not club:
-        return "Club not found", 404
-    club = club[0]
-    
-    club_players = player_df[player_df['current_club_name'] == club ['name']].to_dict(orient='records')
-
-    
-    return render_template('ClubDetail.html', club=club, players=club_players)
+    return render_template('index.html', players=players, clubs=clubs)
 
 if __name__ == '__main__':
     app.run(debug=True)
